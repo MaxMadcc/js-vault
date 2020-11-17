6399886d609b4e8c6831a7ed844200501e6d6083
MaxMadcc <19978097+MaxMadcc@users.noreply.github.com>
2020-11-17 21:32:00 +0800

    Update 115_show_sha1.user.js

--- 115_show_sha1.user.js
+++ 115_show_sha1.user.js
@@ -1,50 +1,72 @@
 // ==UserScript==
-// @name         115显示文件sha1
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        http*://115.com/*wangpan
-// @run-at document-end
-// @grant        none
+// @name         115网盘显示文件SHA1,删除分享按钮
+// @namespace    com.115wangpan.showsha1
+// @version      0.5
+// @description  标题意思
+// @author       YIU
+// @match        http*://115.com/*ct=file*
+// @grant        unsafeWindow
 // ==/UserScript==
 
+var $ = unsafeWindow.$;
 
-function displaySha1() {
-  setTimeout(function () {
-    //$('div.page-center');
-    //$('div.list-cell');
-    $('ul.list-contents li').each(function () {
-      if ($(this).attr('sha1') != undefined)
-      {
-        var vsha1 = $(this).attr('sha1');
-        $(this).children('.file-detail').append('<span>sha1=' + vsha1 + '</span>');
-      }
-    });
-  }, 800);
+//允许重新显示标识
+var canDisplay = 0;
+
+function displaySha1(){
+
+    if(canDisplay > 0)
+    {
+
+        //在文件列表iframe内，只取非文件夹类型，并且含有sha1属性的DOM
+        $('.list-contents li[file_type!="0"][sha1]',$('iframe[rel="wangpan"]')[0]).each(function(){
+            var vsha1 = $(this).attr('sha1');
+            //$(this).children('.file-detail').append('<span gmflag><br>SHA1: ' + vsha1 + '</span>');
+            $(this).children('.file-name').append('<em gmflag1>SHA1: ' + vsha1 + '</em>');
+            $(this).children('.file-name').css('top','5px');
+            $(this).children('.file-name').css('height','50px');
+            $(this).children('.file-name').children('em').css('height','20px');
+        });
+        //禁止重新显示
+        canDisplay = 0;
+
+    }
+
 }
-(function () {
-  displaySha1();
-  $('a[href="javascript:;"]').each(function () {
-    $(this).bind('click', function () {
-      $('ul.list-contents li').each(function () {
-        if ($(this).attr('sha1') != undefined)
-        {
-          var vsha1 = $(this).attr('sha1');
-          $(this).children('.file-detail').append('<span>sha1=' + vsha1 + '</span>');
-        }
-      });
+
+//绑定函数处理方法
+function bindfun(e){
+
+    //等待重新显示
+    if(canDisplay < 1)
+    {
+        canDisplay++;
+
+        setTimeout(function(){
+
+            if($('.list-contents li[file_type!="0"][sha1] em:not([gmflag1])',e.target).parents('li').length > 0)
+            {
+                displaySha1();
+            }
+            canDisplay = 0;
+
+        },100);
+    }
+
+}
+
+
+(function(){
+
+    //绑定文件列表变化事件
+    var bind_list = $('#js_data_list',$('iframe[rel="wangpan"]')[0]).on("DOMSubtreeModified",function(e){
+        bindfun(e);
     });
-  });
-    $('.main-core').bind('click', function () {
-        setTimeout(function () {
-      $('ul.list-contents li').each(function () {
-        if ($(this).attr('sha1') != undefined)
-        {
-          var vsha1 = $(this).attr('sha1');
-          $(this).children('.file-detail').append('<span>sha1=' + vsha1 + '</span>');
-        }
-      });
-	}, 2500);
-   });
-}) ();
+
+    //绑定文件夹点击事件
+    $('.list-contents li[file_type="0"]',$('iframe[rel="wangpan"]')[0]).on("click",function(e){
+        canDisplay = 0;
+        bindfun($(e.target).parents('#js_data_list')[0]);
+    });
+
+})();
